506415a64d04fbbdf5636d466d4c02337b98c368
krasnovsky <mr.krasnovsky@gmail.com>
2025-06-03 13:44:11 +0400

    changed BotConfig

--- src/main/java/CadastrApp/config/BotConfig.java
+++ src/main/java/CadastrApp/config/BotConfig.java
@@ -2,19 +2,21 @@ package CadastrApp.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-import lombok.Data;
-import lombok.Getter;
 
 @Configuration
-@Data
-@Getter
 public class BotConfig {
+
     @Value("${BOT_NAME}")
     private String botName;
 
     @Value("${BOT_TOKEN}")
     private String botToken;
-}
 
+    public String getBotName() {
+        return botName;
+    }
+
+    public String getToken() {
+        return botToken;
+    }
+}
